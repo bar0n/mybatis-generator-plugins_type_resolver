2414ee7704d0cca5bde21caf94c67d67aa8e35e3
Dmitry Baron <dbaron@isg2tech.com>
2019-09-18 09:30:49 +0300

    ZonedDateTime

--- src/main/java/yongfa365/mybatis/generator/plugins/JavaTypeResolverDefaultImpl.java
+++ src/main/java/yongfa365/mybatis/generator/plugins/JavaTypeResolverDefaultImpl.java
@@ -1,5 +1,6 @@
 package yongfa365.mybatis.generator.plugins;
 
+import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
 
 import java.sql.Types;
@@ -11,5 +12,19 @@ public class JavaTypeResolverDefaultImpl extends org.mybatis.generator.internal.
         super();
         typeMap.put(Types.TINYINT, new JdbcTypeInformation("TINYINT",
                 new FullyQualifiedJavaType(Integer.class.getName())));
+    }
+
+    @Override
+    protected FullyQualifiedJavaType calculateTimestampType(IntrospectedColumn column, FullyQualifiedJavaType defaultType) {
+        //return super.calculateTimestampType(column, defaultType);
+        return  new FullyQualifiedJavaType("java.time.ZonedDateTime");
+    }
+
+    @Override
+    protected FullyQualifiedJavaType calculateBigDecimalReplacement(IntrospectedColumn column, FullyQualifiedJavaType defaultType) {
+        if (column.getJdbcType() == Types.NUMERIC){
+
+        }
+        return super.calculateBigDecimalReplacement(column, defaultType);
     }
 }
